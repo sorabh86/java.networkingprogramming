78857cd34dfe14d80530109adb520c80e66e88a0
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-10-02 16:55:52 +0530

    file output stream

---
+++ Streams/src/outstream/Main.java
@@ -0,0 +1,31 @@
+/*
+ * FileOutputStream
+ */
+package outstream;
+
+import java.io.*;
+/**
+ *
+ * @author Sorabh86 <ssorabh.ssharma@gmail.com>
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        try {
+            // second argument true if you want to append data
+            FileOutputStream fileOutput = new FileOutputStream("example2.txt", true);
+            
+            char h = 'H';
+            
+            fileOutput.write(h);
+            
+            String str = "hello java network programming";
+            
+            fileOutput.write(str.getBytes());
+            
+            fileOutput.close();
+        } catch (Exception ex) { 
+            System.out.println(ex.toString());
+        }
+    }
+}
