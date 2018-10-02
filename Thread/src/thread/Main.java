3841984dafb2fb129efae6ecdea14f38a1170fb9
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-10-02 15:56:35 +0530

    2 server

--- Thread/src/thread/Main.java
+++ Thread/src/thread/Main.java
@@ -17,7 +17,9 @@ public class Main {
     public static void main(String[] args) {
         
         ServerThread thread = new ServerThread("Server1");
+        ServerThread thread2 = new ServerThread("Server2");
         thread.start();
+        thread2.start();
     }
     
 }
